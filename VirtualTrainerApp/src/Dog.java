794ba68d0085f6e5457128e1a52335d6280dec8c
mausam.kumravat <mausam.kumravat@YI1005260DT.Yash.com>
2016-09-14 18:50:43 +0530

    I have created dog class

---
+++ VirtualTrainerApp/src/Dog.java
@@ -0,0 +1,18 @@
+//dog class
+public class Dog {
+private String name;
+private String colour;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getColour() {
+	return colour;
+}
+public void setColour(String colour) {
+	this.colour = colour;
+}
+
+}
